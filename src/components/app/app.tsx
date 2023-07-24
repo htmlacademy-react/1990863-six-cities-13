dd842febbf32f650c514de627bdbc313126d22a6
ilianameit <ilianameit@gmail.com>
2023-07-24 11:51:19 +0300

    Добавит маршрутизацию

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,10 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AppRoute } from '../const';
 import MainPage from '../../pages/main-page/main-page';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
+import LoginPage from '../../pages/login-page/login-page';
+import FavoritesPage from '../../pages/favorites-page/favorites-page';
+import OfferPage from '../../pages/offer-page/offer-page';
 
 type AppOffersProps = {
   offersCount: number;
@@ -6,7 +12,17 @@ type AppOffersProps = {
 
 function App({offersCount}: AppOffersProps): React.JSX.Element {
   return (
-    <MainPage offersCount={offersCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Main} element={<MainPage offersCount={offersCount} />}></Route>
+        <Route path={AppRoute.Login} element={<LoginPage />}></Route>
+        <Route path={AppRoute.Favorites} element={<FavoritesPage />}></Route>
+        <Route path={AppRoute.Offer}>
+          <Route path={`${AppRoute.Offer}/:id`} element={<OfferPage />}/>
+        </Route>
+        <Route path='*' element={<NotFoundPage />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
